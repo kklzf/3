aaca0f0728ec6008c6a1448d202d05506382a366
kklzf <137879753+kklzf@users.noreply.github.com>
2023-09-02 20:27:30 +0800

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,3 +1,3 @@
-OPENAI_API_KEY = 'sk-BuUsi3z zgd9W xay3q4eDT3Bl bkFJ8kaBGFjr7HQ5zx m und21'
+OPENAI_API_KEY = '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'
 PINECONE_API_KEY = '0800ccf3-9c87-477e-846e-952a8e27a249'
 PINECONE_API_ENV ='us-west1-gcp-free'

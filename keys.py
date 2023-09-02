8d891bd4739bf88cbf6848984fd31e9c58e57e11
kklzf <137879753+kklzf@users.noreply.github.com>
2023-09-02 12:02:41 +0800

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,3 +1,3 @@
-OPENAI_API_KEY = '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'
+OPENAI_API_KEY = 'sk-BuUsi3z zgd9W xay3q4eDT3Bl bkFJ8kaBGFjr7HQ5zx m und21'
 PINECONE_API_KEY = '0800ccf3-9c87-477e-846e-952a8e27a249'
 PINECONE_API_ENV ='us-west1-gcp-free'

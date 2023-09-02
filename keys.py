e9ac3a3ea18e0272dd497ca9e7ba3822a7cefb71
kklzf <137879753+kklzf@users.noreply.github.com>
2023-09-02 11:41:56 +0800

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,3 +1,3 @@
-OPENAI_API_KEY = '你的key，不是我的key，别想拿我的'
-PINECONE_API_KEY = '你的key，不是我的key，别想拿我的'
-PINECONE_API_ENV = '你的env，不是我的env，别想拿我的'
+OPENAI_API_KEY = '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'
+PINECONE_API_KEY = '0800ccf3-9c87-477e-846e-952a8e27a249'
+PINECONE_API_ENV ='us-west1-gcp-free'
